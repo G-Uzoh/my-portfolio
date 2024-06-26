945f77ef6e519bfe269c59d2ed28dbb9738de461
George Uzoh <ugochukwuuzoh@gmail.com>
2024-06-26 12:36:27 +0300

    add Kotafuz to experience component

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -18,6 +18,29 @@ const Experience = () => {
             >
                 Experience
             </Typography>
+            <Box>
+          <Box component="div">
+            <Typography component="p" fontSize="var(--font-size-lg)">
+              Web Developer Intern, <Typography component="a" sx={{"&:hover": { color: "var(--yellow)" }}} href="https://www.kotafuz.com" target="_blank">Kotafuz</Typography>
+            </Typography>
+            <Typography component="p" fontFamily="var(--font-mono)" fontSize="var(--font-size-xxs)">
+              April 2024 - Present
+            </Typography>
+          </Box>
+          <Box
+            component="ul"
+            className="skillset"
+            sx={{
+              margin: "20px 0 0",
+              overflow: "hidden",
+              listStyle: "none",
+            }}
+          >
+            <Typography component="li">Assist the development team to develop and maintain responsive, user-friendly and visually appealing web applications.</Typography>
+            <Typography component="li">Perform quality assurance tests on sites to ensure cross-browser compatibility and mobile responsiveness.</Typography>
+            <Typography component="li">Participate in agile scrum ceremonies (sprint planning, daily standup, sprint review and sprint retrospective).</Typography>
+          </Box>
+        </Box>
         </Box>
     )
 }
